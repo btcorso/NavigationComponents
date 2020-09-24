7bde6a04be0ed7457eb617e8c0a0bf3fa25e4130
btcorso <uniebcorso@fei.edu.br>
2020-09-24 01:12:11 -0300

    criamos a navegação usando o recurso gráfico do android (NavGraph)

--- app/src/main/java/com/example/navigationcomponents/NewDoacoesFragment.java
+++ app/src/main/java/com/example/navigationcomponents/NewDoacoesFragment.java
@@ -12,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 
 public class NewDoacoesFragment extends Fragment {
@@ -23,27 +24,24 @@ public class NewDoacoesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View rootView = inflater.inflate(R.layout.fragment_new_doacoes, container, false);
-        Button btnRoupa = rootView.findViewById(R.id.btnRoupa);
-        Button btnLivro = rootView.findViewById(R.id.btnLivro);
-        return rootView;
+        return inflater.inflate(R.layout.fragment_new_doacoes, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         navController = Navigation.findNavController(view);
-        Button btnRoupa = view.findViewById(R.id.btnRoupa);
-        Button btnLivro = view.findViewById(R.id.btnLivro);
+        ImageButton roupa = view.findViewById(R.id.imabtnRoupa);
+        ImageButton livro = view.findViewById(R.id.imabtnLivros);
 
-        btnRoupa.setOnClickListener(new View.OnClickListener() {
+        roupa.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 navController.navigate(R.id.action_newDoacoesFragment_to_newRoupaFragment);
             }
         });
 
-        btnLivro.setOnClickListener(new View.OnClickListener() {
+        livro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 navController.navigate(R.id.action_newDoacoesFragment_to_newLivrosFragment);
