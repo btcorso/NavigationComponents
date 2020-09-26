3ca9f89d74af60c2018795fed6af6e85a5b403db
btcorso <uniebcorso@fei.edu.br>
2020-09-26 12:54:12 -0300

    Implementado a união do navigation component and bottomViemBar

--- app/src/main/java/com/example/navigationcomponents/NewDoacoesFragment.java
+++ app/src/main/java/com/example/navigationcomponents/NewDoacoesFragment.java
@@ -1,5 +1,6 @@
 package com.example.navigationcomponents;
-
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.FragmentManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,23 +10,27 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class NewDoacoesFragment extends Fragment {
+
+public class NewDoacoesFragment extends Fragment{
 
     NavController navController = null;
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_new_doacoes, container, false);
+
     }
+
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
@@ -35,6 +40,7 @@ public class NewDoacoesFragment extends Fragment {
         ImageButton livro = view.findViewById(R.id.imabtnLivros);
         ImageButton brinquedo = view.findViewById(R.id.imabtnToy);
         ImageButton movel = view.findViewById(R.id.imabtnMoveis);
+        BottomNavigationView bottomNav = view.findViewById(R.id.nav_bottom);
 
 
         roupa.setOnClickListener(new View.OnClickListener() {
@@ -65,6 +71,8 @@ public class NewDoacoesFragment extends Fragment {
             }
         });
 
+
     }
 
 }
+
