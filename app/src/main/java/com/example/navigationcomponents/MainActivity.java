fc07d341e7ef9668e3dc479719c6b88fe47fdc3c
btcorso <uniebcorso@fei.edu.br>
2020-09-26 12:53:57 -0300

    Implementado a união do navigation component and bottomViemBar

--- app/src/main/java/com/example/navigationcomponents/MainActivity.java
+++ app/src/main/java/com/example/navigationcomponents/MainActivity.java
@@ -1,8 +1,17 @@
 package com.example.navigationcomponents;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.nav_bottom);
+
+        NavController navController = Navigation.findNavController(this,R.id.nav_host_fragment);
+
+        NavigationUI.setupWithNavController(bottomNav, navController);
+
     }
+
 }
